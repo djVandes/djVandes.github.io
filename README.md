411b0c5333933e251eb72fc7c26ccc966ec326b0
DJ <dj.vandesande@gmail.com>
2025-04-21 19:38:21 -0400

    nav bar fixes and image updates

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # Portfolio Website
 
 ## Features 
+* Print shop
 * Engineering projects
 * About Me Section
 * Art Portfolio with subspages for each collection
-
-## Future Add ons
-* Center the fucking art images
-* Commission prices and print prices
 
 
 > Grant! Grant look I made a website
--- js/index.js
+++ js/index.js
@@ -68,7 +68,19 @@ document.addEventListener('DOMContentLoaded', () => {
     // updateCarousel();
     // startAutoplay();
 
+    
 });
 
 
 
+document.addEventListener('click', function (e) {
+    const nav = document.querySelector('.nav');
+    const navToggle = document.querySelector('.nav-toggle');
+
+    // If nav is open and the click is not inside the nav or navToggle, then close nav
+    if (document.body.classList.contains('nav-open') &&
+        !nav.contains(e.target) &&
+        !navToggle.contains(e.target)) {
+      document.body.classList.remove('nav-open');
+    }
+  });
